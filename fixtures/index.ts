c51c4e021703a4ec187edf0e080da1d5f6c87e64
AYYAPPAREDDYKANJULA <142433797+AYYAPPAREDDYKANJULA@users.noreply.github.com>
2026-08-27 16:26:06 +0530

    Add Allure reporting for UI tests

--- fixtures/index.ts
+++ fixtures/index.ts
@@ -1,4 +1,5 @@
-import { test as base, Page } from '@playwright/test';
+import { test as base } from '@playwright/test';
+import type { Page } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
 import { InventoryPage } from '../pages/InventoryPage';
 import { CartPage } from '../pages/CartPage';
